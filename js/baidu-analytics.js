cffd5f981cac985b3f1e80b4081f09e9f437056c
yutaodev <87139411@qq.com>
2026-04-03 15:50:58 +0800

    网站百度收录和统计

---
+++ js/baidu-analytics.js
@@ -0,0 +1,8 @@
+// 百度统计代码
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?89626318ab07a403d4d573c05bf67030";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
